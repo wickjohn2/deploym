eaeff4a8fc56eeba2d6349481275dc8f0e13a3e8
wickjohn2 <johnwick2@gmail.com>
2017-03-01 13:48:53 -0600

    Update create_vm.py

--- create_vm.py
+++ create_vm.py
@@ -1,4 +1,4 @@
-COMPUTE_URL_BASE = 'https://www.googleapis.com/compute/v1/'
+URL_BASE = 'https://www.googleapis.com/compute/v1/'
 
 
 def GenerateConfig(context):
@@ -10,7 +10,7 @@ def GenerateConfig(context):
           'properties': {
               'zone': context.properties['zone'],
               'machineType': context.properties['machineType'],
-              'image': COMPUTE_URL_BASE, + 'projects/albatross-keving-sandbox/global/images/adlib-management-sql-image',
+              'image': URL_BASE + 'projects/albatross-keving-sandbox/global/images/adlib-management-sql-image',
       }
   }]
   return {'resources': resources}
