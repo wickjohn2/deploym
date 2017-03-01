94ab84a915ba784bc47bb8e1a7991a79f9b590db
wickjohn2 <johnwick2@gmail.com>
2017-03-01 14:24:52 -0600

    Update create_vm.py

--- create_vm.py
+++ create_vm.py
@@ -4,12 +4,15 @@ URL_BASE = 'https://www.googleapis.com/compute/v1/'
 def GenerateConfig(context):
   """Create instance"""
 
+  zone = context.properties['zone']
+  machineType = context.properties['machineType']
+  
   resources = [{
           'name': 'management-sql-'+ context.env['deployment'],
           'type': 'compute.v1.instance',
           'properties': {
-              'zone': context.properties['zone'],
-              'machineType': context.properties['machineType'],
+              'zone': zone,
+              'machineType': machineType,
               'image': URL_BASE + 'projects/albatross-keving-sandbox/global/images/adlib-management-sql-image',
               'networkInterfaces': [{
                 'network': URL_BASE + context.env['project'] + '/global/networks/default',
