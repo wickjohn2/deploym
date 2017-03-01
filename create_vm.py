f857355c3321d7c2fa8f4c3141a6216da09d2c7f
wickjohn2 <johnwick2@gmail.com>
2017-03-01 13:47:42 -0600

    Update create_vm.py

--- create_vm.py
+++ create_vm.py
@@ -2,70 +2,15 @@ COMPUTE_URL_BASE = 'https://www.googleapis.com/compute/v1/'
 
 
 def GenerateConfig(context):
-  """Create instance with disks."""
+  """Create instance"""
 
-  datadisk = 'datadisk-'+ context.env['deployment']
   resources = [{
-      'type': 'compute.v1.disk',
-      'name': datadisk,
-      'properties': {
-          'zone': context.properties['zone'],
-          'sizeGb': 10,
-          # Disk type is a full URI.  Example uses pd-standard
-          # but pd-ssd can be used as well
-          'type': ''.join([COMPUTE_URL_BASE, 'projects/',
-                           context.env['project'], '/zones/',
-                           context.properties['zone'],
-                           '/diskTypes/pd-standard'])
-      }
-  }, {
-      'type': 'compute.v1.instance',
-      'name': 'vm-' + context.env['deployment'],
-      'properties': {
-          'zone': context.properties['zone'],
-          'machineType': ''.join([COMPUTE_URL_BASE, 'projects/',
-                                  context.env['project'], '/zones/',
-                                  context.properties['zone'],
-                                  '/machineTypes/f1-micro']),
-          'metadata': {
-              'items': [{
-                  # For more ways to use startup scripts on an instance, see:
-                  # https://cloud.google.com/compute/docs/startupscript
-                  'key': 'startup-script',
-                  'value': '#!/bin/bash\npython -m SimpleHTTPServer 8080'
-              }]
-          },
-          'disks': [{
-              'deviceName': 'boot',
-              'type': 'PERSISTENT',
-              'boot': True,
-              'autoDelete': True,
-              'initializeParams': {
-                  'diskName': 'disk-' + context.env['deployment'],
-                  'sourceImage': ''.join([COMPUTE_URL_BASE, 'projects/',
-                                          'debian-cloud/global/',
-                                          'images/family/debian-8'])}
-          }, {
-              # Specify the data disk to mount. The deviceName can be anything,
-              # but by convention is typically set to the same name.
-              # This is the value is used in
-              # /dev/disk/by-id/google-<deviceName>.
-              # If not set, it will be
-              # /dev/disk/by-id/google-persisent-disk-<number>.
-              'deviceName': 'datadisk',
-              'type': 'PERSISTENT',
-              'source': '$(ref.' + datadisk + '.selfLink)',
-              'autoDelete': True
-          }],
-          'networkInterfaces': [{
-              'network': ''.join([COMPUTE_URL_BASE, 'projects/',
-                                  context.env['project'],
-                                  '/global/networks/default']),
-              'accessConfigs': [{
-                  'name': 'External NAT',
-                  'type': 'ONE_TO_ONE_NAT'
-              }]
-          }]
+          'name': vm,
+          'type': 'compute.v1.instance',
+          'properties': {
+              'zone': context.properties['zone'],
+              'machineType': context.properties['machineType'],
+              'image': COMPUTE_URL_BASE, + 'projects/albatross-keving-sandbox/global/images/adlib-management-sql-image',
       }
   }]
   return {'resources': resources}
