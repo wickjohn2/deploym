a53d3ad6dbfa1aec048820552581db4a32dcfed2
wickjohn2 <johnwick2@gmail.com>
2017-02-28 22:01:59 -0600

    Update create-vm.py

--- create-vm.py
+++ create-vm.py
@@ -2,7 +2,7 @@ def GenerateConfig(context):
   
   resources = {
     'type': 'compute.v1.instance',
-    'name': 'vm-',
+    'name': 'vm-'+ context.env['deployment'],
     'properties': {
           'zone': context.properties['zone'],
           'machineType': context.properties['machinetype'],
